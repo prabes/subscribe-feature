85885b8077d62e78defaba02ba5568043cb620c0
rdeyvil <rdeyvil11@gmail.com>
2019-07-25 23:22:03 +0545

    Install sidekiq and mount the route

--- app/models/concerns/blog_notification.rb
+++ app/models/concerns/blog_notification.rb
@@ -1,16 +1,26 @@
 class BlogNotification 
+
+  include Sidekiq::workers
+  sidekiq_options retry: false
   
-  def self.send_all(blog)
-    # Takes all data from table as a array
-    users = Subscriber.all
+  def perform ()
+    1000.times.puts"okay !"
+  end
+
+  # def self.send_all(blog)
+  #   # Takes all data from table as a array
+  #   users = Subscriber.all
     
 
-    # Takes array loop each 
-    users.each do |user|
-      BlogMailer.blog_notification(user,blog).deliver
-    end
-
-    # calls Each with blog_notification  
-  end
+  #   # Takes array loop each 
+  #   users.each do |user|
+  #     BlogMailer.blog_notification(user,blog).deliver
+  #   end
+    
+  #   # Chunk User in limit of each 500 users ((find_in_batch(diy))
+  #   # raise 500 jobs for each 500 subscribers
+  #   # use sidekiq workers
+  #   # https://github.com/mperham/sidekiq/wiki
+  # end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  require'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
